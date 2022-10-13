253f96c6e05e398e51d6b8d7309516ecf09898b4
Jezreel <jezdtanslc90@gmail.com>
2022-10-13 14:35:04 +0800

    update

---
+++ sources/20221013.js
@@ -0,0 +1,91 @@
+const source = [
+    {
+        name: "Public Talk",
+        class: "m-public",
+        minutes: 30,
+        participant: null
+    },
+    {
+        name: "Watchtower Study",
+        class: "m-public",
+        minutes: 50,
+        participant: null
+    },
+    {
+        name: "Opening Comments",
+        class: "m-mwb",
+        minutes: 1
+    },
+    {
+        name: "Treasures",
+        class: "m-mwb",
+        minutes: 10,
+        participant: null
+    },
+    {
+        name: "Spiritual Gems",
+        class: "m-mwb",
+        minutes: 10,
+        participant: null
+    },
+    {
+        name: "Bible Reading",
+        class: "m-mwb",
+        minutes: 4,
+        participant: null
+    },
+    {
+        name: "Chairman's Comments",
+        class: "m-mwb",
+        minutes: 1,
+        participant: null
+    },
+    {
+        name: "Video: Return Visit ",
+        class: "m-mwb",
+        minutes: 5,
+        participant: null
+    },
+    {
+        name: "Return Visit",
+        class: "m-mwb",
+        minutes: 3,
+        participant: null
+    },
+    {
+        name: "Chairman's Comments",
+        class: "m-mwb",
+        minutes: 1,
+        participant: null
+    },
+    {
+        name: "Bible Study",
+        class: "m-mwb",
+        minutes: 3,
+        participant: null
+    },
+    {
+        name: "Chairman's Comments",
+        class: "m-mwb",
+        minutes: 1,
+        participant: null
+    },
+    {
+        name: "Maintain Positive Attitude",
+        class: "m-mwb",
+        minutes: 15,
+        participant: null
+    },
+    {
+        name: "CBS: llf 7",
+        class: "m-mwb",
+        minutes: 20,
+        participant: null
+    },
+    {
+        name: "Concluding Comments",
+        class: "m-mwb",
+        minutes: 3,
+        participant: null
+    }
+]
