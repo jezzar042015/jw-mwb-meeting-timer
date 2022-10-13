ef391bb663b7d3b27a8bc2ea91c8170fca4c04b7
Jezreel <jezdtanslc90@gmail.com>
2022-10-13 15:12:43 +0800

    20221020

---
+++ sources/20221020.00.js
@@ -0,0 +1,104 @@
+const source = [
+    {
+        name: "Public Talk",
+        class: "m-public",
+        minutes: 30,
+        participant: ""
+    },
+    {
+        name: "Watchtower Study",
+        class: "m-public",
+        minutes: 50,
+        participant: ""
+    },
+    {
+        name: "Opening Comments",
+        class: "m-mwb",
+        minutes: 1,
+        participant: "JT"
+    },
+    {
+        name: "Imitate How Jehovah Exercise Authority",
+        class: "m-mwb",
+        minutes: 10,
+        participant: "JD"
+    },
+    {
+        name: "Spiritual Gems",
+        class: "m-mwb",
+        minutes: 10,
+        participant: "MW"
+    },
+    {
+        name: "Bible Reading",
+        class: "m-mwb",
+        minutes: 4,
+        participant: "RC"
+    },
+    {
+        name: "Chairman's Comments",
+        class: "m-mwb",
+        minutes: 1,
+        participant: "JT"
+    },
+    {
+        name: "First Conversation",
+        class: "m-mwb",
+        minutes: 3,
+        participant: "Jane/Z"
+    },
+    {
+        name: "Chairman's Comments",
+        class: "m-mwb",
+        minutes: 1,
+        participant: "JT",
+    },
+    {
+        name: "Return Visit",
+        class: "m-mwb",
+        minutes: 3,
+        participant: "MG/MO"
+    },
+    {
+        name: "Chairman's Comments",
+        class: "m-mwb",
+        minutes: 1,
+        participant: "JT"
+    },
+    {
+        name: "Bible Study",
+        class: "m-mwb",
+        minutes: 5,
+        participant: "MW"
+    },
+    {
+        name: "Chairman's Comments",
+        class: "m-mwb",
+        minutes: 1,
+        participant: "JT"
+    },
+    {
+        name: "Love is Patiend and Kind",
+        class: "m-mwb",
+        minutes: 10,
+        participant: "JD"
+    },
+    {
+        name: "Local Needs",
+        class: "m-mwb",
+        minutes: 5,
+        participant: "JT"
+    },
+    {
+        name: "CBS: llf 7",
+        class: "m-mwb",
+        minutes: 20,
+        participant: "KD"
+    },
+    {
+        name: "Concluding Comments",
+        class: "m-mwb",
+        minutes: 3,
+        participant: "JT"
+    }
+]
