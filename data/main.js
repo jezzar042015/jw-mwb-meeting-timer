135bb0fe58054513fcefbf79e5393232bb4586da
Jezreel <jezdtanslc90@gmail.com>
2022-12-11 10:53:24 +0800

    public meeting with CO

--- data/main.js
+++ data/main.js
@@ -1,4 +1,4 @@
-const meetingSchedule = "Meetings | Dec 5-11"
+const meetingSchedule = "Meetings | Nov 7-13"
 
 const source = [
     {
@@ -11,36 +11,43 @@ const source = [
     {
         name: "Watchtower Study",
         class: "m-public",
-        minutes: 50,
+        minutes: 30,
         participant: "",
         hasParticipation: true
+    },
+    {
+        name: "Last Service Talk",
+        class: "m-public",
+        minutes: 30,
+        participant: "",
+        hasParticipation: false
     },
     {
         name: "Opening Comments",
         class: "m-mwb",
         minutes: 1,
-        participant: "KD",
+        participant: "MW",
         hasParticipation: false
     },
     {
-        name: 'Wholehearted Effort Results in Abundant Blessings',
+        name: '"Jehovah Made the Unexpected Happen"',
         class: "m-mwb",
         minutes: 10,
         participant: "JT",
         hasParticipation: false
     },
     {
-        name: "Spiritual Gems: 2 Kings 13-15",
+        name: "Spiritual Gems: 2 Kings 7-8",
         class: "m-mwb",
         minutes: 10,
-        participant: "KD",
+        participant: "JD",
         hasParticipation: true
     },
     {
         name: "Bible Reading",
         class: "m-mwb",
         minutes: 4,
-        participant: "AN",
+        participant: "3D",
         hasParticipation: false
 
     },
@@ -48,79 +55,65 @@ const source = [
         name: "Chairman's Comments",
         class: "m-mwb",
         minutes: 1,
-        participant: "KD",
+        participant: "MW",
         hasParticipation: false
 
     },
     {
-        name: "First Conversation",
+        name: "Return Visit: Video",
         class: "m-mwb",
-        minutes: 2,
-        participant: "MO/NB",
-        hasParticipation: false
+        minutes: 5,
+        participant: "MW",
+        hasParticipation: true
 
+    },
+    {
+        name: "Return Visit",
+        class: "m-mwb",
+        minutes: 4,
+        participant: "MD/NB",
+        hasParticipation: false
     },
     {
         name: "Chairman's Comments",
         class: "m-mwb",
         minutes: 1,
-        participant: "KD",
+        participant: "MW",
         hasParticipation: false
     },
     {
         name: "Return Visit",
         class: "m-mwb",
-        minutes: 5,
-        participant: "BD/MG",
+        minutes: 4,
+        participant: "CH/Jane",
         hasParticipation: false
     },
     {
         name: "Chairman's Comments",
         class: "m-mwb",
         minutes: 1,
-        participant: "KD",
+        participant: "MW",
         hasParticipation: false
     },
     {
-        name: "Talk",
+        name: "Governing Body Update #7",
         class: "m-mwb",
-        minutes: 5,
-        participant: "JD",
-        hasParticipation: false
-    },
-    {
-        name: "Chairman's Comments",
-        class: "m-mwb",
-        minutes: 1,
-        participant: "KD",
-        hasParticipation: false
-    },
-    {
-        name: "Jehovah Remembers Our Hard Work",
-        class: "m-mwb",
-        minutes: 10,
-        participant: "JT",
+        minutes: 15,
+        participant: "MW",
         hasParticipation: true
     },
     {
-        name: "Local Needs",
+        name: "CBS: llf 12",
         class: "m-mwb",
-        minutes: 5,
-        participant: "MW",
-        hasParticipation: false
+        minutes: 20,
+        participant: "KD",
+        hasParticipation: true
     },
     {
         name: "Concluding Comments",
         class: "m-mwb",
         minutes: 3,
-        participant: "KD",
-        hasParticipation: false
-    },
-    {
-        name: "Have Faith and Do Not Doubt",
-        class: "m-mwb",
-        minutes: 30,
-        participant: "SCO",
+        participant: "MW",
         hasParticipation: false
     }
 ]
