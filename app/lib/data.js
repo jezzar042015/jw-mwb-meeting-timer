573d58ce7ef234a953048053dec2859ce5533aa2
Jezreel Tan <jezdtanslc90@gmail.com>
2023-10-05 13:36:38 +0800

    10/5 & 10/8

--- app/lib/data.js
+++ app/lib/data.js
@@ -1,4 +1,4 @@
-export const meetingSchedule = "Meetings | Sep 7 & Sep 10"
+export const meetingSchedule = "Meetings | Oct 5 & 8"
 // const meetingSchedule = ""
 
 export const source = [
@@ -6,7 +6,7 @@ export const source = [
         name: "Public Talk",
         class: "m-public",
         minutes: 30,
-        participant: "J. Dy",
+        participant: "K. Mercadala",
         hasParticipation: false
     },
     {
@@ -20,44 +20,43 @@ export const source = [
         name: "Opening Comments",
         class: "m-mwb",
         minutes: 1,
-        participant: "M. Waniwan",
+        participant: "J. Tan",
         hasParticipation: false
     },
     {
-        name: `TREASURES: “He Used His Authority Unselfishly”`,
-        class: "m-mwb",
-        minutes: 10,
-        participant: "K. Dagalea",
-        hasParticipation: false
-    },
-    {
-        name: "Spiritual Gems: Esther 9-10",
+        name: `TREASURES: “Continue to Show the Depth of Your Love for Jehovah”`,
         class: "m-mwb",
         minutes: 10,
         participant: "M. Waniwan",
         hasParticipation: false
     },
     {
-        name: "Bible Reading (Es 9:1-14)",
+        name: "Spiritual Gems: Job 1-3",
+        class: "m-mwb",
+        minutes: 10,
+        participant: "J. Tan",
+        hasParticipation: false
+    },
+    {
+        name: "Bible Reading (Job 3:1-20)",
         class: "m-mwb",
         minutes: 4,
-        participant: "R. Calutan",
+        participant: "E. Redoña",
         hasParticipation: false
-
     },
     {
         name: "Chairman's Comments for Bible Reading",
         class: "m-mwb",
         minutes: 1,
-        participant: "M. Waniwan",
+        participant: "J. Tan",
         hasParticipation: false
 
     },
     {
         name: "First Conversation",
         class: "m-mwb",
-        minutes: 2,
-        participant: "E. Barca | N. Baldesco",
+        minutes: 3,
+        participant: "JC Cuizon | N. Escala",
         hasParticipation: false
 
     },
@@ -65,15 +64,15 @@ export const source = [
         name: "Chairman's Comments for First Conversation",
         class: "m-mwb",
         minutes: 1,
-        participant: "M. Waniwan",
+        participant: "J. Tan",
         hasParticipation: false
 
     },
     {
         name: "Return Visit",
         class: "m-mwb",
-        minutes: 5,
-        participant: "R. Berbes | A. Balleres",
+        minutes: 4,
+        participant: "JI Cuizon | M. Alibadbad",
         hasParticipation: false
 
     },
@@ -81,42 +80,49 @@ export const source = [
         name: "Chairman's Comments for Return Visit",
         class: "m-mwb",
         minutes: 1,
-        participant: "M. Waniwan",
+        participant: "J. Tan",
         hasParticipation: false
     },
     {
-        name: "Bible Study: (5 min.) lff 12:Dig Deeper; 4",
+        name: "Talk: (5 min.) Theme: Teach Effectively Like James—​Be Realistic and Humble",
         class: "m-mwb",
         minutes: 5,
-        participant: "C. Cuizon | Z. Tan",
+        participant: "A. Balleres",
         hasParticipation: false
     },
     {
         name: "Chairman's Comments for Bible Study",
         class: "m-mwb",
         minutes: 1,
+        participant: "J. Tan",
+        hasParticipation: false
+    },
+    {
+        name: `I Thought I Had It All Together`,
+        class: "m-mwb",
+        minutes: 10,
         participant: "M. Waniwan",
         hasParticipation: false
     },
     {
-        name: `“Shepherds Who Work for the Good of Jehovah’s People”`,
+        name: `“Use the JW.ORG Home Page in Your Ministry”`,
         class: "m-mwb",
-        minutes: 15,
-        participant: "J. Dy",
+        minutes: 5,
+        participant: "J. Tan",
         hasParticipation: false
     },
     {
-        name: "Congregation Bible Study: lff 46",
+        name: "Congregation Bible Study: lff 47",
         class: "m-mwb",
         minutes: 20,
-        participant: "J. Tan",
+        participant: "J. Dy",
         hasParticipation: false
     },
     {
         name: "Concluding Comments",
         class: "m-mwb",
         minutes: 3,
-        participant: "M. Waniwan",
+        participant: "J. Tan",
         hasParticipation: false
     }
 ]
