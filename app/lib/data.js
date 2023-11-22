e66373584b69c9704cf2414feb46389751326acb
Jezreel Tan <jezdtanslc90@gmail.com>
2023-11-23 06:27:14 +0800

    11/23

--- app/lib/data.js
+++ app/lib/data.js
@@ -1,4 +1,4 @@
-export const meetingSchedule = "Meeting | Nov 16 & 19"
+export const meetingSchedule = "Meeting | Nov 23 & 26"
 // const meetingSchedule = ""
 
 export const settings = {
@@ -10,65 +10,72 @@ export const source = [
         name: "Public Talk",
         class: "m-public",
         minutes: 30,
-        participant: "J. Tan",
+        participant: "J. Dy",
         hasParticipation: false
     },
     {
         name: "Watchtower Study",
         class: "m-public",
         minutes: 50,
-        participant: "J. Dy",
+        participant: "J. Tan",
         hasParticipation: false
     },
     {
         name: "Opening Comments",
         class: "m-mwb",
         minutes: 1,
-        participant: "M. Waniwan",
-        hasParticipation: false
-    },
-    {
-        name: `TREASURES: “Eliphaz Demonstrates How Not to Give Comfort”`,
-        class: "m-mwb",
-        minutes: 10,
         participant: "K. Dagalea",
         hasParticipation: false
     },
     {
-        name: "Spiritual Gems: Job 15-17",
+        name: `TREASURES: “Never Abandon Your Fellow Worshippers”`,
         class: "m-mwb",
         minutes: 10,
         participant: "J. Dy",
         hasParticipation: false
     },
     {
-        name: "Bible Reading (Job 17:1-16)",
+        name: "Spiritual Gems: Job 18-19",
+        class: "m-mwb",
+        minutes: 10,
+        participant: "J. Tan",
+        hasParticipation: false
+    },
+    {
+        name: "Bible Reading (Job 18:1-21)",
         class: "m-mwb",
         minutes: 4,
-        participant: "A. Danco",
+        participant: "N. Regala III",
         hasParticipation: false
     },
     {
         name: "Chairman's Comments for Bible Reading",
         class: "m-mwb",
         minutes: 1,
-        participant: "M. Waniwan",
+        participant: "K. Dagalea",
         hasParticipation: false
 
     },
     {
-        name: "Return Visit Video",
+        name: "First Conversation",
         class: "m-mwb",
-        minutes: 5,
-        participant: "M. Waniwan",
+        minutes: 3,
+        participant: "E. Barca | R. Balleres",
         hasParticipation: false
 
+    },
+    {
+        name: "Chairman's Comments for First Conversation",
+        class: "m-mwb",
+        minutes: 1,
+        participant: "K. Dagalea",
+        hasParticipation: false
     },
     {
         name: "Return Visit",
         class: "m-mwb",
-        minutes: 3,
-        participant: "M. Olarte | M. Dy",
+        minutes: 4,
+        participant: "C. Waniwan | N. Escala",
         hasParticipation: false
 
     },
@@ -76,49 +83,56 @@ export const source = [
         name: "Chairman's Comments for Return Visit",
         class: "m-mwb",
         minutes: 1,
-        participant: "M. Waniwan",
+        participant: "K. Dagalea",
         hasParticipation: false
     },
     {
-        name: "Bible Study: <i>lff</i> Section 1 Review 6-10",
+        name: "Talk: <i>w20.10</i> 42:10-11​—Theme: Encourage Students to Make Friends in the Congregation",
         class: "m-mwb",
         minutes: 5,
-        participant: "CH Cuizon | EJ Borer",
+        participant: "A. Balleres",
         hasParticipation: false
     },
     {
-        name: "Chairman's Comments for Bible Study",
+        name: "Chairman's Comments for Talk",
         class: "m-mwb",
         minutes: 1,
-        participant: "M. Waniwan",
+        participant: "K. Dagalea",
         hasParticipation: false
     },
     {
-        name: `2023 Governing Body Update #7`,
+        name: `Become Jehovah’s Friend​—Help Others`,
         class: "m-mwb",
-        minutes: 15,
-        participant: "M. Waniwan",
+        minutes: 5,
+        participant: "J. Dy",
         hasParticipation: false
     },
     {
-        name: "Congregation Bible Study: <i>lff</i> 50:1-5; Protect Your Children",
+        name: `“An Arrangement to Comfort Those at Bethel”`,
+        class: "m-mwb",
+        minutes: 10,
+        participant: "J. Tan",
+        hasParticipation: false
+    },
+    {
+        name: "Congregation Bible Study: <i>lff</i> 50:6-7; Summary; Review; Goal",
         class: "m-mwb",
         minutes: 20,
-        participant: "J. Tan",
+        participant: "M. Waniwan",
         hasParticipation: false
     },
     {
         name: "Concluding Comments",
         class: "m-mwb",
         minutes: 3,
-        participant: "M. Waniwan",
+        participant: "K. Dagalea",
         hasParticipation: false
-    }, 
+    },
     {
-        name: "Closin Prayer",
+        name: "Closing Prayer",
         class: "m-mwb",
         minutes: 0,
-        participant: "J. Dy",
+        participant: "M. Waniwan",
         hasParticipation: false
     }
 ]
